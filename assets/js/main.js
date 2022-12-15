19ad9d354a74d56f77a049dfa7ab2a77c467af34
Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>
2022-12-15 22:07:01 +0100

    Update main.js
    Signed-off-by: Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,6 @@
-const counter = document.getElementById('counter');
-const incrementBtn = document.getElementById('increment');
-const resetBtn = document.getElementById('reset');
-const decrementBtn = document.getElementById('decrement');
+let counter = 0
+set interval(() => {
+counter++
+  console.log(counter)
+},1000);
 
-let counterValue = 0;
-
-incrementBtn.addEventListener("click", function (event){
-  counterValue += 1;
-  counter.innerHTML = counterValue;
-  counterStile();
-});
-
-decrementBtn.addEventListener("click", function (event){
-  counterValue -= 1;
-  counter.innerHTML = counterValue;
-  counterStile();
-});
-
-resetBtn.addEventListener("click", function (event){
-  counterValue = 0;
-  counter.innerHTML = counterValue;
-  counter.style.color="rgb(93, 56, 244)"
-});
-
-function counterStile(){
-  if (counterValue <0){
-    counter.style.color="red"
-  }
-  else if (counterValue >0){
-    counter.style.color="green"
-  }
-  else if (counterValue ==0){
-    counter.style.color="rgb(93, 56, 244)"
-  }
-}
