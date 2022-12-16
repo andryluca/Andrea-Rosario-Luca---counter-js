0feed59fca39577d0d9d2b5d67f8c9cf891c8dc0
Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>
2022-12-16 12:09:57 +0100

    Update and rename main.js to script.js
    Signed-off-by: Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//creo le costanti, i pulsanti e le classi(per poterle modificarle in css)
 const operatori = document.querySelector('#operatori');
 const btnInc = document.createElement('button');
 btnInc.innerHTML = '+';
@@ -11,14 +12,13 @@ operatori.appendChild(btnDec);
 btnDec.style.color = 'red'
 btnDec.setAttribute('class','Dec');
 
+//creo l'evento
 let numContainer = document.getElementById("num");
 let value = 0;
-
 btnInc.addEventListener("click", () => {
     value++;
     numContainer.textContent = value;
 });
-
 btnDec.addEventListener("click", () => {
     value--;
     numContainer.textContent = value;
