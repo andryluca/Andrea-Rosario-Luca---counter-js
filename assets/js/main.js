6ec977e6e3489c474e30f9bc3ab6d492ce25409e
Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>
2022-12-16 11:51:27 +0100

    Update main.js
    Signed-off-by: Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
-let counter = 0
-set interval(() => {
-counter++
-  console.log(counter)
-},1000);
+const operatori = document.querySelector('#operatori');
+const btnInc = document.createElement('button');
+btnInc.innerHTML = '+';
+operatori.appendChild(btnInc);
+btnInc.style.color = 'green';
+btnInc.setAttribute('class','Inc');
 
+const btnDec = document.createElement('button');
+btnDec.innerHTML = '−';
+operatori.appendChild(btnDec);
+btnDec.style.color = 'red'
+btnDec.setAttribute('class','Dec');
+
+let numContainer = document.getElementById("num");
+let value = 0;
+btnInc.addEventListener("click", () => {
+    value++;
+    numContainer.textContent = value;
+});
+btnDec.addEventListener("click", () => {
+    value--;
+    numContainer.textContent = value;
+});
