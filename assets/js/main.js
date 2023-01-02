3e26f8d17ed0935638ddd3b77bf02a245b6f6377
Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>
2023-01-02 22:46:33 +0100

    Update main.js
    Signed-off-by: Andrea Rosario Lucà <106505614+andryluca@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
 //creo le costanti, i pulsanti e le classi(per poterle modificarle in css)
-const operatori = document.querySelector('#operatori');
+const operator = document.querySelector('#operatori');
 const btnInc = document.createElement('button');
 btnInc.innerHTML = '+';
-operatori.appendChild(btnInc);
+operator.appendChild(btnInc);
 btnInc.style.color = 'green';
 btnInc.setAttribute('class','Inc');
+btnInc.setAttribute('data-counter','inc');
 
 const btnDec = document.createElement('button');
 btnDec.innerHTML = '−';
-operatori.appendChild(btnDec);
+operator.appendChild(btnDec);
 btnDec.style.color = 'red'
 btnDec.setAttribute('class','Dec');
 
 //creo l'evento
 let numContainer = document.getElementById("num");
 let value = 0;
-btnInc.addEventListener("click", () => {
-    value++;
+
+operator.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.tagName === 'BUTTON') {
+    if (target.classList.contains('Inc')) {
+        value++;
+    } else if (target.classList.contains('Dec')) {
+        value--;
+    }
     numContainer.textContent = value;
-});
-btnDec.addEventListener("click", () => {
-    value--;
-    numContainer.textContent = value;
+    }
 });
